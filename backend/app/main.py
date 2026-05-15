17fffbd6398bcd025a523bb854effe4cfcf5573b
Donagala-Tejaswi <tejaswidongala@gmail.com>
2026-05-15 13:54:18 +0530

    Fixed upload issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
-import shutil
 import os
+import shutil
 
 from app.rag_pipeline import process_pdf, ask_question
 
@@ -16,37 +16,41 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-UPLOAD_FOLDER = "documents"
+UPLOAD_FOLDER = "uploads"
+
+# create uploads folder automatically
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 
 @app.get("/")
 def home():
-    return {
-        "message": "RAG System Running"
-    }
+    return {"message": "RAG Backend Running"}
 
 
 @app.post("/upload")
 async def upload_pdf(file: UploadFile = File(...)):
+    try:
+        file_path = os.path.join(UPLOAD_FOLDER, file.filename)
 
-    file_path = f"{UPLOAD_FOLDER}/{file.filename}"
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
 
-    with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+        process_pdf(file_path)
 
-    process_pdf(file_path)
+        return {
+            "message": "PDF uploaded successfully",
+            "filename": file.filename
+        }
 
-    return {
-        "message": "PDF uploaded successfully"
-    }
+    except Exception as e:
+        return {"error": str(e)}
 
 
 @app.get("/ask")
 def ask(query: str):
+    try:
+        answer = ask_question(query)
+        return {"answer": answer}
 
-    answer = ask_question(query)
-
-    return {
-        "answer": answer
-    }
+    except Exception as e:
+        return {"error": str(e)}
