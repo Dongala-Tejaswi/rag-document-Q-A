be270ece1a910701a53a9837f5723045d9af5d22
Donagala-Tejaswi <tejaswidongala@gmail.com>
2026-05-15 12:45:59 +0530

    Added ask API endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,6 +7,7 @@ from app.rag_pipeline import process_pdf, ask_question
 
 app = FastAPI()
 
+# CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -16,56 +17,36 @@ app.add_middleware(
 )
 
 UPLOAD_FOLDER = "documents"
-
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 
 @app.get("/")
 def home():
-    return {"message": "RAG System Running"}
+    return {
+        "message": "RAG System Running"
+    }
 
 
 @app.post("/upload")
 async def upload_pdf(file: UploadFile = File(...)):
 
-    try:
-        file_path = f"{UPLOAD_FOLDER}/{file.filename}"
+    file_path = f"{UPLOAD_FOLDER}/{file.filename}"
 
-        with open(file_path, "wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
 
-        print("PDF Saved")
+    process_pdf(file_path)
 
-        process_pdf(file_path)
-
-        print("PDF Processed")
-
-        return {
-            "message": "PDF uploaded successfully"
-        }
-
-    except Exception as e:
-        print("UPLOAD ERROR:", str(e))
-        return {"error": str(e)}
+    return {
+        "message": "PDF uploaded successfully"
+    }
 
 
 @app.get("/ask")
 def ask(query: str):
 
-    try:
-        print("Question:", query)
+    answer = ask_question(query)
 
-        answer = ask_question(query)
-
-        print("Generated Answer:", answer)
-
-        return {
-            "answer": answer
-        }
-
-    except Exception as e:
-        print("ASK ERROR:", str(e))
-
-        return {
-            "error": str(e)
-        }
+    return {
+        "answer": answer
+    }
